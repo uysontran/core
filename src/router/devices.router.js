acca58daa7d91da28218ea821057d2ef15c6e18f
Noobmaster69-bug <tranuyson@gmail.com>
2022-05-25 09:29:04 +0000

    fixed some bug

--- src/router/devices.router.js
+++ src/router/devices.router.js
@@ -1,5 +1,6 @@
 const Router = require("express").Router();
-const { get, post, provision } = require("../controller/devices");
+const { get, post, provision,provisionConfirm } = require("../controller/devices");
+Router.post("/provision/confirm", provisionConfirm);
 Router.get("/provision", provision);
 Router.post("/", post);
 Router.get("/", get);
