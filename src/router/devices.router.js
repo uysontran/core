fd6f726159c2f522cad5e7ca38fa3675d70537f4
root <root@Miles.localdomain>
2022-05-31 10:18:48 +0700

    update api

--- src/router/devices.router.js
+++ src/router/devices.router.js
@@ -1,7 +1,14 @@
 const Router = require("express").Router();
-const { get, post, provision ,info} = require("../controller/devices");
-Router.get("/info",info)
+const {
+  get,
+  post,
+  provision,
+  info,
+  provisionConfirm,
+} = require("../controller/devices");
+Router.get("/info", info);
 Router.get("/provision", provision);
+Router.get("provision/confirm", provisionConfirm);
 Router.post("/", post);
 Router.get("/", get);
 module.exports = Router;
